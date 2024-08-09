09838cd99b138cc88f6fb98fa06e88023cb8af25
unknown <frexcho998@gmail.com>
2024-08-09 12:49:06 +0300

    chore: implement ocelot gateway

---
+++ server/QuizMaster.Server/QuizMaster.Server.QuestionService/Controllers/QuestionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizMaster.Server.QuestionService.Models;
+
+namespace QuizMaster.Server.QuestionService.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class QuestionController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<QuestionDTO>>> Get()
+        {
+            return Ok(new List<QuestionDTO> {
+                new QuestionDTO() {
+                     Id = 1,
+                     QuestionText = "test"
+                }
+            });
+        }
+    }
+}
---
+++ server/QuizMaster.Server/QuizMaster.Server.QuestionService/Models/QuestionDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizMaster.Server.QuestionService.Models
+{
+    public class QuestionDTO
+    {
+        public int Id { get; set; }
+        public string QuestionText { get; set; }
+    }
+}
